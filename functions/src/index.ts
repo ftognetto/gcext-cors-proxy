91d28a16c74cd64147bc0ab0f65a561fb2081452
fabriziotognetto <info@quantos.it>
2021-06-16 20:37:37 +0200

    better cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,22 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
 import got from 'got';
 
 export const corsProxy = functions.handler.https.onRequest( async (req, res) => {
 
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
-    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.set('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD');
+    res.header('Access-Control-Allow-Headers', req.header('access-control-request-headers'));
 
     if (req.method === 'OPTIONS') {
         // Send response to OPTIONS requests
-        res.set('Access-Control-Allow-Methods', 'GET');
-        res.set('Access-Control-Allow-Headers', 'Content-Type');
-        res.set('Access-Control-Max-Age', '3600');
-        res.status(204).send('');
-        res.end();
+        res.send();
     } else {
         const url = req.params && req.params[0];
         if (!url) { 
             res.status(400).json({ 'message': 'Missing parameters' }); 
             res.end();
         } else {
-            const response = await got.get(`${url}`);
-            res.status(200).send(response.body);
-            res.end();
+            (await got.get(`${url}`)).pipe(res);
         }
     }
 
